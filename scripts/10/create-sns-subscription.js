759a542040d0fcc194511ea55b2d8835e8dda7c2
Mog <amg.seiphemo@gmail.com>
2023-09-13 02:59:28 +0200

    The endpoint(phone number in this case) is stored in AWS KMS. We request KMS to decrypt the phone number instead of hard coding it.

--- scripts/10/create-sns-subscription.js
+++ scripts/10/create-sns-subscription.js
@@ -2,15 +2,29 @@
 const {
   SubscribeCommand
 } = require('@aws-sdk/client-sns')
-const { sendSNSCommand: sendCommand } = require('./helpers')
+
+const { 
+  sendKMSCommand,
+  sendSNSCommand: sendCommand 
+} = require('./helpers')
+
+const {
+  DecryptCommand,
+  KMSClient
+} = require('@aws-sdk/client-kms')
 
 // Declare local variables
 const type = 'sms'
-const endpoint = '/* TODO: Add your mobile number with country code */'
-const topicArn = '/* TODO: Add your sns topic arn */'
+const encryptedPhoneNumber = 'AQICAHiSg2VEiyNrQ/WBsj+bSNRDAPLcX7E7wpAZ7h4JStghtwEV/I3MAOXzc2qYnt7OyI9gAAAAajBoBgkqhkiG9w0BBwagWzBZAgEAMFQGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM0YWMEaaClX4jF7ixAgEQgCeGvJ/m3bC9xrCqx9NKCjmEBeZZfbEJa+A9WTJxMjV47RwNmTfl4L0='
+const encryptionKeyAlias = 'alias/phone-number'
+const topicArn = 'arn:aws:sns:us-east-1:620502844819:hamster-topic'
 
 async function execute () {
   try {
+    const r = await decryptPhoneNumber(encryptedPhoneNumber, encryptionKeyAlias)
+    const plainTextBase64 = r['Plaintext']
+    const endpoint = Buffer.from(plainTextBase64, 'base64').toString('ascii')
+    
     const response = await createSubscription(type, topicArn, endpoint)
     console.log(response)
   } catch (err) {
@@ -18,8 +32,26 @@ async function execute () {
   }
 }
 
-function createSubscription (type, topicArn, endpoint) {
-  // TODO: Subscribe to topic
+async function createSubscription (type, topicArn, endpoint) {
+  const params = {
+    Protocol: type,
+    TopicArn: topicArn,
+    Endpoint: endpoint
+  }
+  const command = new SubscribeCommand(params)
+  return sendCommand(command)
+}
+
+async function decryptPhoneNumber(encryptedPhoneNumber, encryptionKeyAlias) {
+  const params = {
+    CiphertextBlob: Buffer.from(encryptedPhoneNumber, 'base64'),
+    KeyId: encryptionKeyAlias,
+    EncryptionAlgorithm: "SYMMETRIC_DEFAULT",
+    DryRun: false
+  }
+
+  const command = new DecryptCommand(params)
+  return sendKMSCommand(command)
 }
 
 execute()
--- scripts/10/helpers.js
+++ scripts/10/helpers.js
@@ -1,17 +1,23 @@
 const { CloudWatchClient } = require('@aws-sdk/client-cloudwatch')
 const { SNSClient } = require('@aws-sdk/client-sns')
+const { KMSClient } = require('@aws-sdk/client-kms')
 
+async function sendKMSCommand(command) {
+  const client  = new KMSClient({ region: "us-east-1", profile: "Admin" })
+  return client.send(command)
+}
 async function sendSNSCommand (command) {
-  const client = new SNSClient({ region: process.env.AWS_REGION })
+  const client = new SNSClient({ region: 'us-east-1', profile: 'Admin' })
   return client.send(command)
 }
 
 async function sendCloudWatchCommand (command) {
-  const client = new CloudWatchClient({ region: process.env.AWS_REGION })
+  const client = new CloudWatchClient({ region: 'us-east-1', profile: 'Admin' })
   return client.send(command)
 }
 
 module.exports = {
   sendCloudWatchCommand,
-  sendSNSCommand
+  sendSNSCommand,
+  sendKMSCommand
 }
